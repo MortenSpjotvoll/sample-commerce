167041c3ea752c99b1963f617392580541b79119
Morten Spjøtvoll <morten.spjotvoll@hansencx.com>
2021-12-05 11:33:57 +0100

    Added header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shoppage/ShopPage';
-
+import Header from './components/header/Header.jsx';
 export default function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop/" element={<ShopPage />} />
