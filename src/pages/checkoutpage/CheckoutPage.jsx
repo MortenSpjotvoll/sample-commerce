94923b3ae081b3ea80658c449c261e97ebe94711
Morten Spjøtvoll <morten.spjotvoll@hansencx.com>
2022-01-23 19:59:00 +0100

    Added stripe integration

--- src/pages/checkoutpage/CheckoutPage.jsx
+++ src/pages/checkoutpage/CheckoutPage.jsx
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { selectCartItems, selectCartTotal } from "redux/cart/cartSelector";
 import CheckoutItem from "components/checkoutitem/CheckoutItem";
+import StripeButton from "components/stripebutton/StripeButton";
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -28,6 +29,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>Total: ${total}</span>
     </div>
+    <div className='test-warning'>
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/23 - CVV: 123
+    </div>
+    <StripeButton price={total} />
   </div>
 );
 
